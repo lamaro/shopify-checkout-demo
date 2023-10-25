abc7c321f0d6d580b0c25f19d2a574dcd6aa9e60
Leandro Amaro Garcia <leanamaro@gmail.com>
2023-10-25 20:37:00 -0300

    added axios wip

--- src/lib/shopify.js
+++ src/lib/shopify.js
@@ -1,29 +1,26 @@
+import axios from "axios";
+
 const domain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN;
 const storefrontAccessToken =
   process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESSTOKEN;
 
 async function ShopifyData(query) {
   const URL = `https://${domain}/api/2023-10/graphql.json`;
-  console.log(storefrontAccessToken);
-  const options = {
-    endpoint: URL,
-    method: "POST",
-    headers: {
-      "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query }),
-  };
-
   try {
-    const data = await fetch(URL, options).then((response) => {
-      return response.json();
-    });
-
-    return data;
+    const runQuery = await axios.post(
+      URL,
+      { query },
+      {
+        headers: {
+          "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return runQuery.data;
   } catch (error) {
-    throw new Error("Products not fetched");
+    throw new Error("Products not fetched", error);
   }
 }
 
@@ -129,11 +126,9 @@ export async function getProduct(handle) {
 
 export async function createCheckout(lineItems) {
   const lineItemsObject = lineItems.map((item) => {
-    return `{
-      variantId: "${item.id}",
-      quantity:  ${item.quantity}
-    }`;
+    return `{variantId: "${item.gid}",quantity: ${item.quantity}}`;
   });
+  console.log(lineItemsObject);
   const query = `
     mutation {
       checkoutCreate(input: {
@@ -146,13 +141,17 @@ export async function createCheckout(lineItems) {
       }
     }`;
 
-  const response = await ShopifyData(query);
-  console.log(response);
-  const checkout = response.data.checkoutCreate.checkout
-    ? response.data.checkoutCreate.checkout
-    : [];
+  try {
+    const response = await ShopifyData(query);
+    console.log(response);
+    const checkout = response.data.checkoutCreate.checkout
+      ? response.data.checkoutCreate.checkout
+      : [];
 
-  return checkout;
+    return checkout;
+  } catch (error) {
+    console.log("Error create checkout", error);
+  }
 }
 
 export async function updateCheckout(id, lineItems) {
@@ -183,7 +182,6 @@ export async function updateCheckout(id, lineItems) {
   }`;
 
   const response = await ShopifyData(query);
-
   const checkout = response.data.checkoutLineItemsReplace.checkout
     ? response.data.checkoutLineItemsReplace.checkout
     : [];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const router = useRouter();
-  const { getProducts, createShopifyCheckout } = useShopContext();
+  const { getProducts, createShopifyCheckout, createShopifyProduct } =
+    useShopContext();
 
   useEffect(() => {
     getProducts();
@@ -16,15 +17,26 @@ export default function Home() {
 
   const goToCheckout = () => {
     createShopifyCheckout([
-      { id: "gid://shopify/ProductVariant/46921440756002", quantity: 2 },
-      { id: "gid://shopify/ProductVariant/46921440756002", quantity: 5 },
+      { gid: "gid://shopify/ProductVariant/46921440756002", quantity: 2 },
+      { gid: "gid://shopify/ProductVariant/46921440756002", quantity: 5 },
     ]);
   };
+
+  const createSampleProduct = () => {
+    const prodCreated = createShopifyProduct([
+      { sku: "2223", option1:"test1", title: "Apple Watch1", price: 200.99, inventory: 3 },
+      { sku: "ooo", option1:"test2", title: "Apple Watch2", price: 200.99, inventory: 3 },
+      { sku: "j00kk", option1:"tes3", title: "Apple Watch3", price: 200.99, inventory: 3 },
+    ]);
+    console.log(prodCreated);
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <button onClick={goToCheckout}>Create Checkout</button>
+      <button onClick={createSampleProduct}>Create Product</button>
     </main>
   );
 }
